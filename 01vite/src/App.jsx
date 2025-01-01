fb6412cb66815c70262bf9ccfe1d60ad877b2296
sam0690 <118650996+sam0690@users.noreply.github.com>
2025-01-01 21:45:28 +0545

    learn react

---
+++ 01vite/src/App.jsx
@@ -0,0 +1,22 @@
+
+import Chai from './chai'
+
+
+function App() {
+
+  return (
+    <div>
+     
+
+      <h1>CHAI</h1>
+      <p>chai khana manlagyo.</p>
+
+      <Chai/>
+
+
+    </div>
+    
+  )
+}
+
+export default App
